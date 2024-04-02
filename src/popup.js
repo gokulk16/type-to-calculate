d45b39efba6a5889251bcf945b46db6e0a4fc7c9
Gokul K <gokulk@Gokuls-MacBook-Air.local>
2024-04-02 12:48:29 +0530

    initial commit for the extension with working code

---
+++ src/popup.js
@@ -0,0 +1,23 @@
+("use strict");
+console.log("This is a popup that shows on clicking the extension!");
+
+import * as win from "./js/window.js";
+
+let defaultWindowSize = {
+  width: 550,
+  height: 420,
+};
+
+function getUid() {
+  return "type_to_calculate_" + Math.random().toString(36).slice(-8);
+}
+
+async function createNewDocument() {
+  let id = getUid();
+  await win.newWindow(id, defaultWindowSize.width, defaultWindowSize.height);
+  console.log("created new window - - - with id: ", id);
+  // await new Promise((resolve) => setTimeout(resolve, 30000));
+  window.close();
+}
+
+createNewDocument();
