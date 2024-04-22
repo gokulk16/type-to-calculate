739ad54e9c06391f37092eeb653a6357d993804c
Gokul K <gokulk@Gokuls-MacBook-Air.local>
2024-04-22 17:32:33 +0530

    performance optimisation: adding only the currency tokens that are required. reduced the initial load time of 7 seconds to 10ms

--- src/js/regex.js
+++ src/js/regex.js
@@ -6,3 +6,6 @@ export const VARIABLE = /^\s*([\p{L}_]+) *(=) *([^=]+)$/gmu;
 export const WORD = /[\p{L}_]+/gu;
 export const SUFFIX = /(\d+(?:\.\d+)?)([KkMB]{1}\b)/g;
 export const TAB = /\t/g;
+export const CURRENCY_CONVERSION = /\d+\s*(\w{3})\s+(to|in)\s+(\w{3})\b/g;
+export const CURRENCY_CONVERSION_KEYWORDS = /\s*(\w{3})\s+(to|in)\s+(\w{3})\b/g; // should be a subset of CURRENCY_CONVERSION
+
